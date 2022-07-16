df91d00a079a855cc6c11e45fe30a36b0bdbbcee
Eric <webtester3000@gmail.com>
2022-07-16 15:32:20 -0400

    Add files via upload

---
+++ guacamole/js/data.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        id: 1,
+        name: "Hoodies",
+        urlImages: "./ecommerce/featured1.png",
+        stock: 10,
+        price: 14.00,
+    },
+    {
+        id: 2,
+        name: "Shirts",
+        urlImages: "./ecommerce/featured2.png",
+        stock: 15,
+        price: 24.00,
+    },
+    {
+        id: 3,
+        name: "Sweatshirts",
+        urlImages: "./ecommerce/featured3.png",
+        stock: 20,
+        price: 24.00,
+    }
+];
